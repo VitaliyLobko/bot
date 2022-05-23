0caa3177997118577abe937f94c7783b4efc37c9
Vitaliy Lobko <vitaliy.lobko@gmail.com>
2022-05-23 12:28:36 +0300

    0.01

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 def decorator(func):
     def wrapper(*args):
         try:
+            if len(args[0])>2:
+                raise IndexError
             return func(*args)
         except IndexError:
-            print ('waiting from you: add phone')
+            print('waiting from you: add name phone')
         except KeyError:
             print('can`t find this key')
     return wrapper
@@ -13,35 +15,36 @@ def decorator(func):
 def hello(*args):
     return "How can I help you?"
 
+
 @decorator
 def add(param):
 
-    if ADDRESS_BOOK.get(param[0]) != None:
+    if ADDRESS_BOOK.get(param[0]) is not None:
         print('already exist! rewrite? y/n')
-        anwser = input().strip()
-        if anwser == 'y':
+        anw = input().strip()
+        if anw == 'y':
             ADDRESS_BOOK[param[0]] = param[1]
         return f'changed, now: {param[0]} - {param[1]}'
     else:
         ADDRESS_BOOK[param[0]] = param[1]
         return f'added: {param[0]} - {param[1]}'
 
+
 @decorator
 def change(param):
-    if len(param) < 2:
-        return 'waiting from you: name phone'
     ADDRESS_BOOK[param[0]] = param[1]
     return f'changed, now: {param[0]} - {param[1]}'
 
+
 @decorator
 def phone(param):
-    ph = (ADDRESS_BOOK.get(param[0]))
+    ph = (ADDRESS_BOOK[param[0]])
     return f'{param} -  {ph}'
+
 
 @decorator
 def show_all(*args):
-    print(ADDRESS_BOOK)
-
+    return ADDRESS_BOOK
 
 
 ADDRESS_BOOK = {'vitaliy': '+380 050 616 33 99', 'andriy': '+380 050 646 33 92'}
@@ -50,7 +53,7 @@ COMMANDS = {
     hello: "hello",
     add: "add",
     phone: "phone",
-    show_all: "show", #show all
+    show_all: "show",
     change: "change"
 }
 
@@ -59,13 +62,14 @@ def main():
     while True:
         command,  *param = list(input(">>>").lower().split())
 
-        if command == '.' or 'good bye':
+        if command == '.' or command == 'good bye':
             print('Good bye!')
             break
 
         for k, v in COMMANDS.items():
             if v == command:
                 print(k(param))
+
 
 if __name__ == "__main__":
     main()
